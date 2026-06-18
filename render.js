6fb1c03f80453a12c9e9b2da7e5f02f1a49cf313
DaryaAIV <daryaaiv1@gmail.com>
2026-06-18 17:36:53 +0300

    Add  Extensions-dropdown for selecting saved users and implement renderDropdown function

--- render.js
+++ render.js
@@ -1,41 +1,51 @@
-const Renderer = function() {
+const Renderer = function () {
 
-    function renderMainUser(mainUser){
+    function renderMainUser(mainUser) {
         const container = document.querySelector("#main-user-container");
         container.innerHTML = `<img src="${mainUser.picture}" alt="user picture">
                                 <h2>${mainUser.name}</h2>
                                 <h2>${mainUser.city}, ${mainUser.state} </h2>`;
     }
-    
-    function renderQuote(quote){
+
+    function renderQuote(quote) {
         const container = document.querySelector("#quote-container");
         container.innerHTML = `<h2>Favorite quote:</h2>
                                 <p>${quote}</p>`;
     }
 
-    function renderPokemon(pokemon){
+    function renderPokemon(pokemon) {
         const container = document.querySelector("#pokemon-container");
-         container.innerHTML = `<img src="${pokemon.image}" alt="pokemon image">
+        container.innerHTML = `<img src="${pokemon.image}" alt="pokemon image">
                                 <h3>${pokemon.name} </h3>`;
     }
 
-    function renderAboutMe(text){
+    function renderAboutMe(text) {
         const container = document.querySelector("#about-me-container");
         container.innerHTML = `<h2>About me:</h2>
                                 <p>${text}</p>`;
     }
 
     //generates an HTML list of friends and injects it into the container
-    function renderFriends(friendsList){
+    function renderFriends(friendsList) {
         const container = document.querySelector("#friends-container");
         let friendsStr = "";
-        for (let i = 0; i < friendsList.length; i++){
+        for (let i = 0; i < friendsList.length; i++) {
             friendsStr += `<li>${friendsList[i]}</li>`;
         }
         container.innerHTML = friendsStr;
     }
 
-    function renderPage(data){
+    function renderDropdown(usersObj) {
+        const container = document.querySelector("#saved-users-dropdown");
+        let usersStr = "";
+        // for in loop iterates through the object keys, saved usernames
+        for (const userName in usersObj) {
+            usersStr += `<option value = "${userName}">${userName}</option>`;
+        }
+        container.innerHTML = usersStr;
+    }
+
+    function renderPage(data) {
         renderMainUser(data.mainUser);
         renderQuote(data.kanyeQuote);
         renderPokemon(data.pokemon);
@@ -43,6 +53,7 @@ const Renderer = function() {
         renderFriends(data.friends);
     }
     return {
-        renderPage: renderPage
+        renderPage: renderPage,
+        renderDropdown: renderDropdown
     };
 };
